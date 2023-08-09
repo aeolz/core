321cf77691f1eca9be999aab9c4bab03f406a04e
Dann <vmakichyan0@gmail.com>
2023-08-09 23:15:12 +0400

    package

--- src/objects/DailyScheduler.ts
+++ src/objects/DailyScheduler.ts
@@ -10,6 +10,14 @@ export type DailySchedulerOptions = {
    * New Check time
    */
   setLastCheckTime: (lastCheck: number) => Promise<void>
+  /**
+   * 24 hour format
+   */
+  targetHour: number
+  /**
+   * Whether or not to cache first time getLastCheckTime, to avoid external db calls
+   */
+  cacheGetter?: boolean
 }
 
 export class DailyScheduler {
@@ -18,11 +26,16 @@ export class DailyScheduler {
   private setLastCheckTime: (lastCheck: number) => Promise<void>
   private checkEverySeconds: number
   private loop: Loop
+  private targetHour: number
+  private _lastCheck: number
+  private cacheGetter: boolean
 
   constructor(options: DailySchedulerOptions) {
     this.getLastCheckTime = options.getLastCheckTime
     this.setLastCheckTime = options.setLastCheckTime
     this.checkEverySeconds = options.checkEverySeconds
+    this.targetHour = options.targetHour
+    this.cacheGetter = !!options.cacheGetter
   }
 
   register(fn: () => Promise<void> | void) {
@@ -38,14 +51,34 @@ export class DailyScheduler {
     return this
   }
 
+  private async cachedLastCheckTime(): Promise<number> {
+    if (this.cacheGetter) {
+      if (this._lastCheck) {
+        return this._lastCheck
+      } else {
+        this._lastCheck = await this.getLastCheckTime()
+        return this._lastCheck
+      }
+    } else {
+      return await this.getLastCheckTime()
+    }
+  }
+
   async schedule() {
-    const lastCheckTime = await this.getLastCheckTime()
+    const lastCheckTime = await this.cachedLastCheckTime()
 
     const currentTime = Date.now()
     const oneDayInMilliseconds = 24 * 60 * 60 * 1000 // 24 hours
-    if (currentTime - lastCheckTime >= oneDayInMilliseconds) {
+
+    const today = new Date(currentTime)
+    today.setHours(this.targetHour, 0, 0, 0)
+    if (
+      currentTime - lastCheckTime >= oneDayInMilliseconds &&
+      currentTime >= today.getTime()
+    ) {
       this.checkFunction.forEach((fn) => fn())
-      this.setLastCheckTime(currentTime)
+      this._lastCheck = today.getTime()
+      this.setLastCheckTime(today.getTime())
       // Update the last check time
       // You need to implement the logic to update the last check time using your storage mechanism.
     }
